a1b7c8b5e5e2e0375a65c406fa9416083969515e
David Petterson <david.petterson@weightwatchers.com>
2016-09-09 16:41:38 -0400

    Add message to About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,7 +11,10 @@ var React = require('react');
 // Refactor abov using a stateless functional component
 var About = (props) => {
   return(
-    <h3>About Component</h3>
+    <div>     
+      <h3>About</h3>
+      <p>Welcome to the About page</p>
+    </div>
   );
 };
 

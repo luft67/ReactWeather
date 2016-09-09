f53c0729f83b4ace0444301450736b2f2cdaaa3f
David Petterson <david.petterson@weightwatchers.com>
2016-09-09 16:13:24 -0400

    Prep repo for heroku

--- server.js
+++ server.js
@@ -3,10 +3,21 @@ var express = require('express');
 // create app
 var app = express();
 
+const PORT = process.env.PORT || 3000;
+
+// middleware
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+}
+
 // tell which folder to use
 app.use(express.static('public'));
 
 // start server (port, function)
-app.listen(3000, function() {
-  console.log('express server is running on port 3000');
+app.listen(PORT, function() {
+  console.log('express server is running on port ' + PORT);
 });

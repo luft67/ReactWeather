174f94a15ba61264cbb741fbe5dd8134191ec00a
David Petterson <david.petterson@weightwatchers.com>
2016-09-09 15:28:56 -0400

    Update copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -11,7 +11,7 @@ var React = require('react');
 // refactor above using stateless functional component
 var Examples = (props) => {
   return (
-    <h3>Examples Component</h3>
+    <h3>Examples Component!</h3>
   ); 
 };
 

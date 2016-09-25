0115d6611c5dc76ae2ae3c7e0aa313ffd858256a
David Petterson <david.petterson@weightwatchers.com>
2016-09-25 08:45:54 -0400

    This commit is just a test to test git

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,25 +1,28 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-// var Nav = React.createClass({
-//   render: function() {
-//     return(
-//       <div>
-//         <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-//         <Link to="about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-//         <Link to="examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-//       </div>       
-//     );
-//   }
-// });
-
 var Nav = () => {
   return(
-    <div>
-      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-      <Link to="about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-      <Link to="examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-    </div>       
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">React Weather App</li>
+          <li>
+            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+          </li>
+          <li>
+            <Link to="about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+          </li>
+          <li>
+            <Link to="examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className="top-bar-right">
+      </div>
+
+    </div>  
   );
 };
 
